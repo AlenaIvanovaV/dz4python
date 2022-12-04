5b8ebd4dc88a7ec69d582d994390efeb3fbd44b2
ivanova_av10 <angel40995@inbox.ru>
2022-12-05 00:34:23 +0300

    исправлено

--- python_dz.py
+++ python_dz.py
@@ -1,23 +1,20 @@
-def func_name (a,*args):
-   full_name = a.__name__.title().replace('_', ' ')
-   print(full_name,*args)
+def func_name(a, *args):
+    full_name = a.__name__.title().replace('_', ' ')
+    print(full_name, *args)
 
 
 def open_browser(browser_name):
-   func_name (open_browser,browser_name)
+    func_name(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-
-    func_name (go_to_companyname_homepage,page_url)
-
+    func_name(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-   func_name(find_registration_button_on_login_page, page_url,button_text)
+    func_name(find_registration_button_on_login_page, page_url, button_text)
 
 
 open_browser(browser_name="Chrome")
 go_to_companyname_homepage(page_url="https://dzen.ru/?yredirect=true")
 find_registration_button_on_login_page(page_url="https://dzen.ru/?yredirect=true", button_text="click")
-
